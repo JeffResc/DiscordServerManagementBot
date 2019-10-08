78ac4678be8d485aa5170d5b458c741e6127cf56
Lucas <lukaszrabiec@gmail.com>
2019-10-09 00:21:15 +0200

    Added command for displaying top five processes

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ exports.settings = {
 };
 
 exports.modules = {
-  enabled: ['ping', 'uptime', 'diskInfo', 'memInfo', 'reboot', 'sysInfo', 'cpuType', 'ipInfo', 'nettest'],
+  enabled: ['ping', 'uptime', 'diskInfo', 'memInfo', 'reboot', 'sysInfo', 'cpuType', 'ipInfo', 'nettest', 'topProc'],
 };
