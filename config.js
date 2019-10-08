d99455ca8fe35d3ea3bcc9a088d5d078e468de97
Jeff Rescignano <jeff@jeffresc.com>
2019-10-08 17:00:32 -0400

    Fix indentation

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.settings = {
-	defaultCommandPrefix: "!",
+  defaultCommandPrefix: "!",
   discordClientID: "", // https://discordapp.com/developers/applications/
   discordBotToken: "", // https://discordapp.com/developers/applications/
-	authorizedUsers: ["", ""], // User ID of the Discord member(s) that are authorized to use this bot. Choose wisely.
+  authorizedUsers: ["", ""], // User ID of the Discord member(s) that are authorized to use this bot. Choose wisely.
   showUserUnauthorizedMessage: true, // Will show a "user unauthorized to perform this command" when they are not part of the authorizd users. If false, the bot will just ignore them.
   showLink: true // Show the link to add the bot to a server. You can probably disable this after first use.
 };
